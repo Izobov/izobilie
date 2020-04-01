941e7ff6e8c16df3a095d5fa35414cd4d48b7fcd
iizobov <iizobov@xbsoftware.by>
2020-04-01 16:07:11 +0300

    Updates

---
+++ precache-manifest.288ae111ddd24557cf46fdfbd1247214.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61148a7dda540c1bbb42b977fde4868",
+    "url": "/izobilie/index.html"
+  },
+  {
+    "revision": "a61f04957d4967979655",
+    "url": "/izobilie/static/css/2.0988ea1f.chunk.css"
+  },
+  {
+    "revision": "b7caa9ee6f51daa6d97d",
+    "url": "/izobilie/static/css/main.94e03002.chunk.css"
+  },
+  {
+    "revision": "a61f04957d4967979655",
+    "url": "/izobilie/static/js/2.4bbb456a.chunk.js"
+  },
+  {
+    "revision": "73dd1405d79e31f707da18d53ccd162b",
+    "url": "/izobilie/static/js/2.4bbb456a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7caa9ee6f51daa6d97d",
+    "url": "/izobilie/static/js/main.4a522539.chunk.js"
+  },
+  {
+    "revision": "50d0b126eee67367c30f",
+    "url": "/izobilie/static/js/runtime-main.24ebddd7.js"
+  },
+  {
+    "revision": "a5efe84608b9afaf600bc1071bf31bc4",
+    "url": "/izobilie/static/media/Logo.a5efe846.png"
+  },
+  {
+    "revision": "8e0f817af7b4ec91b331359177c5e07f",
+    "url": "/izobilie/static/media/basket3.8e0f817a.png"
+  },
+  {
+    "revision": "4ace96871ab275b5d5eb9a082c76b7cf",
+    "url": "/izobilie/static/media/cancel.4ace9687.png"
+  },
+  {
+    "revision": "484ec6c8d8817f526813e72c2cb0c20b",
+    "url": "/izobilie/static/media/img1.484ec6c8.jpg"
+  },
+  {
+    "revision": "f795552bf8aaf085297273a0a714bad1",
+    "url": "/izobilie/static/media/img2.f795552b.jpg"
+  },
+  {
+    "revision": "8c01d1774e955373747757d30cf4cf30",
+    "url": "/izobilie/static/media/img3.8c01d177.jpg"
+  },
+  {
+    "revision": "ad3cdaa97e65e8d5772bafc918c91523",
+    "url": "/izobilie/static/media/img4.ad3cdaa9.jpg"
+  },
+  {
+    "revision": "a8ea3e80bd688edf8bb06ce712efe397",
+    "url": "/izobilie/static/media/img5.a8ea3e80.jpg"
+  },
+  {
+    "revision": "dca2961968c97d2983db0c68cd062804",
+    "url": "/izobilie/static/media/instagram.dca29619.svg"
+  },
+  {
+    "revision": "941c9ae0a6b074dccf594491cc992202",
+    "url": "/izobilie/static/media/izobilie.941c9ae0.png"
+  },
+  {
+    "revision": "b1db8a262f0ecec92483b17fc0601abe",
+    "url": "/izobilie/static/media/izobilie4.b1db8a26.png"
+  },
+  {
+    "revision": "8c4871d1581ceb93405b4e33c9aa60d0",
+    "url": "/izobilie/static/media/search.8c4871d1.png"
+  }
+]);
